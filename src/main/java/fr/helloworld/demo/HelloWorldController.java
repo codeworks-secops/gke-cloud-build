a6561e3f70a55d363eba8f0c686be5b8b6628877
Hamza <hamza.el-yaaqoubi-ext@yrnet.com>
2021-10-29 09:54:53 +0200

    init commit

---
+++ src/main/java/fr/helloworld/demo/HelloWorldController.java
@@ -0,0 +1,19 @@
+package fr.helloworld.demo;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @RequestMapping("/")
+    public String hello() {
+        return "Hello you!";
+    }
+
+    @RequestMapping("/{name}")
+    public String helloByName(@PathVariable String name) {
+        return "Hello " + name + "!";
+    }
+}
