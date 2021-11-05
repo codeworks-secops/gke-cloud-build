5f67141cc284dd7df67d4089213b21e74870486a
Hamza <hamza.el-yaaqoubi-ext@yrnet.com>
2021-11-05 15:02:04 +0100

    add new cloud build configuration

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 FROM openjdk:8-jdk-alpine
 ARG JAR_FILE=target/*.jar
 COPY ${JAR_FILE} app.jar
-EXPOSE 6666
+EXPOSE 9090
 ENTRYPOINT ["java","-jar","/app.jar"]
--- src/main/java/fr/helloworld/demo/HelloWorldController.java
+++ src/main/java/fr/helloworld/demo/HelloWorldController.java
@@ -9,11 +9,13 @@ public class HelloWorldController {
 
     @RequestMapping("/")
     public String hello() {
+        System.out.println("Hello method is invoked !");
         return "Hello you !!!!";
     }
 
     @RequestMapping("/{name}")
     public String helloByName(@PathVariable String name) {
+        System.out.println("Hello method is invoked with the name : " + name);
         return "Hello " + name + "!";
     }
 }
